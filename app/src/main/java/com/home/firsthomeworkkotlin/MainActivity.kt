61753f597539be70a9a0e33767a2adb2d4a8ad18
BacuJluo <78647116+BacuJluo@users.noreply.github.com>
2022-03-22 23:47:26 +0300

    Added Data class, Button and setOnClickListener.

--- app/src/main/java/com/home/firsthomeworkkotlin/MainActivity.kt
+++ app/src/main/java/com/home/firsthomeworkkotlin/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.home.firsthomeworkkotlin
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.home.firsthomeworkkotlin.R
+import android.widget.Button
+
+import android.widget.TextView
+import com.home.firsthomeworkkotlin.datasource.MyDataClass
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myData = MyDataClass(1, "Москва")
+        val mySecondData = myData.copy()
+        mySecondData.town="Москва"
+        mySecondData.weather=9
+        val textView:TextView = findViewById(R.id.textView)
+        val button:Button = findViewById(R.id.btnPush)
+        button.setOnClickListener{
+            textView.text = "Город: "+mySecondData.town+"\nТемпература: "+mySecondData.weather+" градусов!"
+
+        }
     }
+
+
+
+
 }
+
+
---
+++ app/src/main/java/com/home/firsthomeworkkotlin/datasource/MyDataClass.kt
@@ -0,0 +1,5 @@
+package com.home.firsthomeworkkotlin.datasource
+
+import android.provider.ContactsContract
+
+data class MyDataClass(var weather:Int, var town:String)
