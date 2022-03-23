3abe7d3410297734af34566022c9eccf8ef07fb7
BacuJluo <78647116+BacuJluo@users.noreply.github.com>
2022-03-24 00:08:10 +0300

    Finished first homework

--- app/src/main/java/com/home/firsthomeworkkotlin/datasource/MyDataClass.kt
+++ app/src/main/java/com/home/firsthomeworkkotlin/datasource/MyDataClass.kt
@@ -1,5 +1,21 @@
 package com.home.firsthomeworkkotlin.datasource
 
-import android.provider.ContactsContract
+import android.util.Log
+
 
 data class MyDataClass(var weather:Int, var town:String)
+
+
+object MyTestObject {
+
+    fun getMyTest()="Hello my friend"
+
+    fun getTestForEach() {
+        val list = listOf(1, 2, 3, 4)
+        //Инициализация forEach'a
+        list.forEach {
+            Log.d("myLogs", "$it Hello my friend")
+        }
+    }
+
+}
