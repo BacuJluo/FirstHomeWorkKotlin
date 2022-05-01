139e7d06b1e7850752484ed4f9250c814641907d
BacuJluo <78647116+BacuJluo@users.noreply.github.com>
2022-05-01 20:17:24 +0300

    ДЗ 5. немного изменил ошибки 2, изменил ключ

--- app/src/main/java/com/home/firsthomeworkkotlin/repository/WeatherLoader.kt
+++ app/src/main/java/com/home/firsthomeworkkotlin/repository/WeatherLoader.kt
@@ -6,7 +6,9 @@ import android.util.Log
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import com.google.gson.Gson
+import com.home.firsthomeworkkotlin.BuildConfig
 import java.io.BufferedReader
+import java.io.FileNotFoundException
 import java.io.InputStreamReader
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
@@ -14,46 +16,70 @@ import javax.net.ssl.HttpsURLConnection
 class WeatherLoader(private val onServerResponseListener: OnServerResponse, val view: View) {
 
     fun loadWeather(lat: Double, lon: Double) {
-        Thread {
-            val urlText = "https://api.weather.yandex.ru/v2/informers?lat=$lat&lon=$lon"
-            val uri = URL(urlText)
-            //посылаем запрос на сервер
-            val urlConnection: HttpsURLConnection =
-                (uri.openConnection() as HttpsURLConnection).apply {
-                    connectTimeout = 1000
-                    readTimeout = 1000
-                    addRequestProperty("X-Yandex-API-Key", "dcfed173-ae28-4526-a15e-1ed1b3e24ad1")
-                }
 
-            val headers = urlConnection.headerFields
-            //коды
-            val responseCode = urlConnection.responseCode
-            //Буфферизация обращения к серверу
-            val buffer = BufferedReader(InputStreamReader(urlConnection.inputStream))
-            val weatherDTO: WeatherDTO = Gson().fromJson(buffer, WeatherDTO::class.java)
-
-            when {
-                responseCode >= 500 -> {
-                    Snackbar.make(view,
-                        "Упс. Что-то пошло не так..\nошибка приложения сервера",
-                        Snackbar.LENGTH_LONG).show()
-                }
-                responseCode >= 400 -> {
-                    Snackbar.make(view,
-                        "Упс. Что-то пошло не так..\nошибка приложения клиента",
-                        Snackbar.LENGTH_LONG).show()
-                }
-                responseCode < 300 -> {
-                    //Смотрящий на главный поток
-                    Handler(Looper.getMainLooper()).post { onServerResponseListener.onResponse(weatherDTO) }
-
-                }
+        val urlText = "https://api.weather.yandex.ru/v2/informers?lat=$lat&lon=$lon"
+        val uri = URL(urlText)
+        //посылаем запрос на сервер
+        val urlConnection: HttpsURLConnection =
+            (uri.openConnection() as HttpsURLConnection).apply {
+                connectTimeout = 1000
+                readTimeout = 1000
+                addRequestProperty("X-Yandex-API-Key", BuildConfig.WEATHER_API_KEY)
+                //addRequestProperty("X-Yandex-API-Key", BuildConfig.WEATHER_API_KEY_ERROR) //Для проверки ошибки FileNotFoundException
             }
 
-            urlConnection.disconnect()
-            Log.d("@@@", "Закрыли коннект")
+            Thread {
+                //коды ошибок
+                val responseCode = urlConnection.responseCode
+                val responseMessage = urlConnection.responseMessage
 
+                try {
+                    //Буфферизация обращения к серверу
+                    val buffer = BufferedReader(InputStreamReader(urlConnection.inputStream))
+                    val weatherDTO: WeatherDTO = Gson().fromJson(buffer, WeatherDTO::class.java)
 
-        }.start()
+                    val headers = urlConnection.headerFields
+
+                    val serverSide = 500
+                    val clientSide = 400
+                    val responseOk = 300
+
+                    when {
+                        responseCode >= serverSide -> {
+                            Snackbar.make(view,
+                                "Упс. Что-то пошло не так..\n$responseMessage ошибка сервера",
+                                Snackbar.LENGTH_LONG).show()
+                        }
+                        responseCode >= clientSide -> {
+                            Snackbar.make(view,
+                                "Упс. Что-то пошло не так..\n$responseMessage ошибка клиента",
+                                Snackbar.LENGTH_LONG).show()
+                        }
+                        responseCode < responseOk -> {
+                            //Смотрящий на главный поток
+                            Handler(Looper.getMainLooper()).post {
+                                onServerResponseListener.onResponse(weatherDTO)
+                            }
+                            Snackbar.make(view,
+                                "Все хорошо\n$",
+                                Snackbar.LENGTH_LONG).show()
+                        }
+                    }
+                    urlConnection.disconnect()
+                    Log.d("@@@", "Закрыли коннект")
+
+                }catch (e: FileNotFoundException){
+                    Log.d("111", "Ошибка, файл не найден")
+                    println("Ошибка, файл не найден")
+                    Snackbar.make(view,
+                        "Упс. Что-то пошло не так..\n$responseMessage ошибка клиента",
+                        Snackbar.LENGTH_LONG).show()
+                }
+                finally {
+                    urlConnection.disconnect()
+                    Log.d("@@@", "Закрыли коннект 1")
+                }
+            }.start()
+
     }
 }
