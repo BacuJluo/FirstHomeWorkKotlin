7393530afb09a48021a5f7d9d64519ff580221c5
BacuJluo <78647116+BacuJluo@users.noreply.github.com>
2022-05-01 18:13:19 +0300

    ДЗ 5. немного изменил ошибки

--- app/src/main/java/com/home/firsthomeworkkotlin/repository/WeatherLoader.kt
+++ app/src/main/java/com/home/firsthomeworkkotlin/repository/WeatherLoader.kt
@@ -4,16 +4,14 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import com.google.gson.Gson
-import com.google.gson.JsonSyntaxException
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 
-class WeatherLoader(private val onServerResponseListener:OnServerResponse, val view: View) {
+class WeatherLoader(private val onServerResponseListener: OnServerResponse, val view: View) {
 
     fun loadWeather(lat: Double, lon: Double) {
         Thread {
@@ -26,22 +24,35 @@ class WeatherLoader(private val onServerResponseListener:OnServerResponse, val v
                     readTimeout = 1000
                     addRequestProperty("X-Yandex-API-Key", "dcfed173-ae28-4526-a15e-1ed1b3e24ad1")
                 }
-            try {
-                val headers = urlConnection.headerFields
-                //коды
-                val responseCode = urlConnection.responseCode
-                //Буфферизация обращения к серверу
-                val buffer = BufferedReader(InputStreamReader(urlConnection.inputStream))
 
-                val weatherDTO:WeatherDTO = Gson().fromJson(buffer, WeatherDTO::class.java)
-                //Смотрящий на главный поток
-                Handler(Looper.getMainLooper()).post { onServerResponseListener.onResponse(weatherDTO) }
-            } catch (e : JsonSyntaxException){
-                    Snackbar.make(view, "Упс. Что-то пошло не так..\nСкорее всего ошибка приложения", Snackbar.LENGTH_LONG).show()
-            } finally  {
-                urlConnection.disconnect()
-                Log.d("@@@", "Закрыли коннект")
+            val headers = urlConnection.headerFields
+            //коды
+            val responseCode = urlConnection.responseCode
+            //Буфферизация обращения к серверу
+            val buffer = BufferedReader(InputStreamReader(urlConnection.inputStream))
+            val weatherDTO: WeatherDTO = Gson().fromJson(buffer, WeatherDTO::class.java)
+
+            when {
+                responseCode >= 500 -> {
+                    Snackbar.make(view,
+                        "Упс. Что-то пошло не так..\nошибка приложения сервера",
+                        Snackbar.LENGTH_LONG).show()
+                }
+                responseCode >= 400 -> {
+                    Snackbar.make(view,
+                        "Упс. Что-то пошло не так..\nошибка приложения клиента",
+                        Snackbar.LENGTH_LONG).show()
+                }
+                responseCode < 300 -> {
+                    //Смотрящий на главный поток
+                    Handler(Looper.getMainLooper()).post { onServerResponseListener.onResponse(weatherDTO) }
+
+                }
             }
+
+            urlConnection.disconnect()
+            Log.d("@@@", "Закрыли коннект")
+
 
         }.start()
     }
